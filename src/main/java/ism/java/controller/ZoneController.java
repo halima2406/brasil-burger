4593310116fa5b151ec53721c424538e81538d17
Halima Lena Camara <halimalenac@gmail.com>
2025-12-14 18:40:24 +0000

    feat(zone): Lister les zones

---
+++ src/main/java/ism/java/controller/ZoneController.java
@@ -0,0 +1,43 @@
+package ism.java.controller;
+
+import ism.java.entity.Zone;
+import ism.java.service.ZoneService;
+import ism.java.view.ZoneView;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ZoneController {
+    
+    private ZoneService zoneService;
+    private ZoneView zoneView;
+    
+    public ZoneController(ZoneService zoneService, Scanner scanner) {
+        this.zoneService = zoneService;
+        this.zoneView = new ZoneView(scanner);
+    }
+    
+    public void run() {
+        boolean back = false;
+        while (!back) {
+            zoneView.afficherMenu();
+            int choix = zoneView.saisirChoix();
+            
+            switch (choix) {
+                case 1:
+                    lister();
+                    break;
+                case 0:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Choix invalide");
+            }
+        }
+    }
+    
+    private void lister() {
+        List<Zone> zones = zoneService.getAllZonesIncludingArchived();
+        zoneView.afficherListe(zones);
+    }
+}
