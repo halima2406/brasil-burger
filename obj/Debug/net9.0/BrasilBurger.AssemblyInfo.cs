be7ff263770e4417e2850358d757d8cc1fc2d087
Halima Lena Camara <halimalenac@gmail.com>
2025-12-19 22:07:04 +0000

    Ajout du Layout principal

--- obj/Debug/net9.0/BrasilBurger.AssemblyInfo.cs
+++ obj/Debug/net9.0/BrasilBurger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilBurger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b541ce886b11f7eba0f9db072920c010ae1fcbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378825e9968a947b71d702aea3bc391dca2bc887")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilBurger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilBurger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
