046238b7d8fc39d48c82016f9cfc8370edcc1972
Halima Lena Camara <halimalenac@gmail.com>
2025-12-19 14:07:43 +0000

    Ajout du modèle Produit

---
+++ Models/Produit.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BrasilBurger.Models
+{
+    [Table("produit")]
+    public class Produit
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("nom")]
+        public string Nom { get; set; } = string.Empty;
+
+        [Column("prix")]
+        public decimal Prix { get; set; }
+
+        [Column("image")]
+        public string? Image { get; set; }
+
+        [Column("type_produit")]
+        public string TypeProduit { get; set; } = string.Empty;
+
+        [Column("type_complement")]
+        public string? TypeComplement { get; set; }
+
+        [Column("est_archive")]
+        public bool EstArchive { get; set; } = false;
+    }
+}
