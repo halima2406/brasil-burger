bbefd38c5e1f4f02ae17e5b03e7a7cd3b8d97fa5
Halima Lena Camara <halimalenac@gmail.com>
2025-12-19 17:10:50 +0000

    Ajout du HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,63 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using BrasilBurger.Models;
+using BrasilBurger.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace BrasilBurger.Controllers;
-
-public class HomeController : Controller
+namespace BrasilBurger.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private ApplicationDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+       
+        public IActionResult Index()
+        {
+            
+            var produits = _context.Produits.ToList();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            
+            var burgers = new List<BrasilBurger.Models.Produit>();
+            int count = 0;
+            foreach (var p in produits)
+            {
+                if (p.TypeProduit == "BURGER" && p.EstArchive == false)
+                {
+                    burgers.Add(p);
+                    count++;
+                    if (count >= 4) break;
+                }
+            }
+
+         
+            var tousLesMenus = _context.Menus
+                .Include(m => m.Burger)
+                .Include(m => m.Boisson)
+                .Include(m => m.Frite)
+                .ToList();
+
+            
+            var menus = new List<BrasilBurger.Models.Menu>();
+            count = 0;
+            foreach (var m in tousLesMenus)
+            {
+                if (m.EstArchive == false)
+                {
+                    menus.Add(m);
+                    count++;
+                    if (count >= 4) break;
+                }
+            }
+
+            ViewBag.Burgers = burgers;
+            ViewBag.Menus = menus;
+
+            return View();
+        }
     }
 }
