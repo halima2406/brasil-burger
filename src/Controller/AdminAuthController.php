89a54101c896d0d5bc2d8b8cda63386ba4f705de
Halima Lena Camara <halimalenac@gmail.com>
2025-12-29 17:16:47 +0000

    feat: Create AdminAuthController class

--- src/Controller/AdminAuthController.php
+++ src/Controller/AdminAuthController.php
@@ -3,14 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AdminAuthController extends AbstractController
 {
-    #[Route('/', name: 'app_root')]
-    public function redirectToLogin(): Response
-    {
-        return $this->redirectToRoute('app_admin_login');
-    }
+    
 }
