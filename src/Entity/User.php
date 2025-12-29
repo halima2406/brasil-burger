1b1e9b1c5f765ba00df0b6c4e7826535a9c58832
Halima Lena Camara <halimalenac@gmail.com>
2025-12-29 20:46:42 +0000

    feat: Add User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
+//#[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'user')]
 #[ORM\InheritanceType("JOINED")]
 #[ORM\DiscriminatorColumn(name: "dtype", type: "string")]
